e53f4869fdee5f2537757195f87f0cb308c79016
Hristo Dzhambov <dzhambov@hotmail.com>
2020-03-13 14:31:57 -0700

    add a _Layout.cshtml page.

--- OrderTracker/Controllers/VendorController.cs
+++ OrderTracker/Controllers/VendorController.cs
@@ -39,7 +39,7 @@ namespace OrderTracker.Controllers
     }
 
     [HttpPost("/vendors/{vendorId}/orders")]
-    public ActionResult Create(int vendorId, string orderTitle, string orderDescription, double orderPrice, int orderDate)
+    public ActionResult Create(int vendorId, string orderTitle, string orderDescription, double orderPrice, string orderDate)
     {
       Dictionary<string, object> model = new Dictionary<string, object>();
       Vendor foundVendor = Vendor.Find(vendorId);
